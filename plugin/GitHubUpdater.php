7aed8b0fe4f45e28a6babb205e1f7a8f6b3eec06
Blaine Moore <blaine@runtowin.com>
2024-08-13 11:41:49 -0400

    Removing test code

--- plugin/GitHubUpdater.php
+++ plugin/GitHubUpdater.php
@@ -714,7 +714,6 @@ class GitHubUpdater
 
     private function getPluginChangeLog(): string 
     {
-        //return '<p>paragraph</p><h1>heading 1</h1><h2>heading 2</h2><h3>heading 3</h3><h4>heading 4</h4><h5>heading 5</h5><h6>heading 6</h6><b>bold</b><strong>strong</strong><i>italics</i><em>emphasis</em><ol><li>order 1</li><li>order 2</li></ol><ul><li>bullet 1</li><li>bullet 2</li><s>strikethrough</s><blockquote>blockquote</blockquote><a href="https://google.com">google.com</a><code>code</code>';
         $this->loadRepositoryVersions();
         
         $changeLog = '<em>Changelog not currently available.</em>';
